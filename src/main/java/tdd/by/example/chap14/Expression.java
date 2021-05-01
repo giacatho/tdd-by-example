d49a37f1679c4492646d8d906929169244452881
giacatho <giacatho@gmail.com>
2021-05-01 11:32:02 +0800

    Chapter 14

---
+++ src/main/java/tdd/by/example/chap14/Expression.java
@@ -0,0 +1,5 @@
+package tdd.by.example.chap14;
+
+public interface Expression {
+	Money reduce(Bank bank, String to);
+}
---
+++ src/main/java/tdd/by/example/chap14/Sum.java
@@ -0,0 +1,23 @@
+package tdd.by.example.chap14;
+
+public class Sum implements Expression {
+	Money augend;
+	Money addend;
+	
+	public Sum(Money augend, Money addend) {
+		this.augend = augend;
+		this.addend = addend;
+	}
+	
+	public Money reduce(String to) {
+		double amount = augend.amount + addend.amount;
+		return new Money(amount, to);
+	}
+
+	@Override
+	public Money reduce(Bank bank, String to) {
+		double amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
+		return new Money(amount, to);
+	}
+	
+}
