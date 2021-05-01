ee201cf71106c1eab53c692956b55e867c677e40
giacatho <giacatho@gmail.com>
2021-05-01 12:40:10 +0800

    Chapter 15

---
+++ src/main/java/tdd/by/example/chap15/Bank.java
@@ -0,0 +1,22 @@
+package tdd.by.example.chap15;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+	private Map<Pair, Integer> rates = new HashMap<>();
+	
+	Money reduce(Expression source, String to) {
+		return source.reduce(this, to);
+	}
+	
+	int rate (String from, String to) {
+		if (from.equals(to)) return 1;
+		
+		return rates.get(new Pair(from, to));
+	}
+	
+	void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
+	}
+}
---
+++ src/main/java/tdd/by/example/chap15/Expression.java
@@ -0,0 +1,5 @@
+package tdd.by.example.chap15;
+
+public interface Expression {
+	Money reduce(Bank bank, String to);
+}
